1889aab449fa0f8d7b7bf034c618f0ad5042b26a
gianmarcog <marco.giretti@web.de>
2020-11-20 15:42:38 +0100

    Added information about the code

--- game.js
+++ game.js
@@ -3,38 +3,54 @@ const optionButtonsElement = document.getElementById('options-buttons')
 
 let state = {}
 
+// Start function which get called when the game starts
 function startGame() {
+    // The current state is empty. 
     state = {}
+    // This function is responsible for the Question-View
     showTextNode(1)
 }
 
+// Function which is going to show the text
 function showTextNode(textNodeIndex) {
+    // If the transferred value textNodeIndex a id in the Array texNodes is the const textNode getting the value
     const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    // The text from the Array with the Id from textNode get transfer in the textElement with the (Html)-Id 'text'
     textElement.innerText = textNode.text
+    // Function get called 
+    // Buttons from the prev. view gettinig deletet
     while (optionButtonsElement.firstChild) {
         optionButtonsElement.removeChild(optionButtonsElement.firstChild)
     }
-
+    // From the choosed textNodeIndex the difference answer options getting showed
     textNode.options.forEach(option => {
+        // Button get created
         if (showOption(option)) {
             const button = document.createElement('button')
             button.innerText = option.text
             button.classList.add('btn')
+            // Eventlistener which says 'if you click on this option the option get choosen'
             button.addEventListener('click', () => selectOption(option))
+            // The choosen buuttons appends
             optionButtonsElement.appendChild(button)
         } 
     })
 }
 
+// Function which return the state from the current option
 function showOption(option) {
     return option.requiredState == null || option.requiredState(state)
 }
 
+// Function which recognize the selected option
 function selectOption(option) {
+    // the const nextTextId indicate the next Text which shows up
     const nextTextNodeId = option.nextText
+    // if the value less than zero the game is restarting cause you lost the game
     if(nextTextNodeId <= 0) {
         return startGame()
     }
+    // state get overwrite from (state and option.setState)
     state = Object.assign(state, option.setState)
     showTextNode(nextTextNodeId)
 }
@@ -60,6 +76,7 @@ const textNodes = [{
         text: 'You venture forth in search of answers to where you are when you come across a merchant.',
         options: [{
                 text: 'Trade the goo for a sword',
+                // The value requiredState includes the current state with it's values
                 requiredState: (currentState) => currentState.blueGoo,
                 setState: {
                     blueGoo: false,
